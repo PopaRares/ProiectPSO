faf6508b417fdadab0b2ea89d57f88e03cb61e47
Rares Popa <raresp98@gmail.com>
2020-10-27 22:28:52 +0200

    replaced cond_compare with thread_compare cause I cant read the manual properly

--- threads/synch.c
+++ threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_insert_ordered (&sema->waiters, &thread_current ()->elem, cond_compare, NULL);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, thread_compare, NULL);
       thread_block ();
     }
   sema->value--;
@@ -295,7 +295,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_insert_ordered (&cond->waiters, &waiter.elem, cond_compare, NULL);
+  list_insert_ordered (&cond->waiters, &waiter.elem, thread_compare, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
