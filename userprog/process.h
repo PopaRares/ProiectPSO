19202e4dab12cabd10b0eecb85954341025d06a6
PanteaCalinMariusIonut <pantea.calin.mi@gmail.com>
2020-12-16 12:42:05 +0200

    multithread

--- userprog/process.h
+++ userprog/process.h
@@ -10,6 +10,17 @@ struct opened_file
     struct list_elem file_elem;
 };
 
+struct uthread_args {
+	char th_name[16];	// the name of the thread
+	int th_pid;			// the pid the thread belongs to
+	void* th_fc_addr;	// user function address
+	int th_fc_arg;		// user function argument
+	int th_no;			// current thread number
+	uint32_t *th_pagedir;	// page directory of the process
+};
+
+tid_t process_uthread_execute (struct uthread_args *th_arg);
+
 static int fd_counter;
 static struct list files;
 
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -12,9 +12,17 @@
 #include "process.h"
 #include "pagedir.h"
 
+#include "threads/synch.h"
+
+static struct semaphore join_sema[TH_NO];
+static struct uthread_args process_uthreads_args[TH_NO];
+static tid_t process_uthreads_tids[TH_NO];
+static int process_uthread_status[TH_NO];
+
 static void syscall_handler (struct intr_frame *);
 void verify_addresses(void*, int);
 void unexpected_exit();
+static crt_thread_id = 0;
 
 void
 unexpected_exit()
@@ -59,7 +67,14 @@ syscall_handler (struct intr_frame *f UNUSED)
   struct opened_file* op_f;
   int fd;
   char* buffer;
-  
+
+  //int syscall_no = ((int*)f->esp)[0];
+	int no;
+	char *buf;
+	int waited_tid;
+	int *status, th_status, i;
+
+
   verify_addresses(addr, 1);
 
 	int syscall_no = (int*)addr++;
@@ -239,6 +254,80 @@ syscall_handler (struct intr_frame *f UNUSED)
         f->eax = -1;
       }
       break;
+
+      case SYS_UTHREAD_CREATE:
+			printf ("SYS_UTHREAD_CREATE system call from thread %d!\n", thread_current()->tid);
+
+			process_uthreads_args[crt_thread_id].th_fc_addr = ((int*)f->esp)[1];
+			process_uthreads_args[crt_thread_id].th_fc_arg = ((int*)f->esp)[2];
+
+		  snprintf(process_uthreads_args[crt_thread_id].th_name,
+					sizeof(process_uthreads_args[crt_thread_id].th_name),
+					"th-%d", crt_thread_id);
+
+		process_uthreads_args[crt_thread_id].th_pid = thread_current()->pid;
+
+			process_uthreads_args[crt_thread_id].th_pagedir = thread_current()->pagedir;
+
+			
+			process_uthreads_args[crt_thread_id].th_no = crt_thread_id;
+
+			process_uthreads_tids[crt_thread_id] = process_uthread_execute(&process_uthreads_args[crt_thread_id]);
+
+			if (process_uthreads_tids[crt_thread_id] != -1) {
+				sema_init(&join_sema[crt_thread_id],0);
+
+				f->eax = crt_thread_id;
+			} else {
+				f->eax = -1;
+			}
+
+			crt_thread_id++;
+			return;
+		case SYS_UTHREAD_JOIN:
+			printf ("SYS_UTHREAD_JOIN system call from thread %d!\n", thread_current()->tid);
+
+			waited_tid = ((int*)f->esp)[1];
+			status = (int*)((int*)f->esp)[2];
+
+			if (process_uthreads_tids[waited_tid] != -1) {
+				
+				sema_down(&join_sema[waited_tid]);
+
+				*status = process_uthread_status[waited_tid];
+				f->eax = 0;
+			} else
+				/* Return error - non existent thread */
+				f->eax = -1;
+
+			return;
+		case SYS_UTHREAD_EXIT:
+			printf ("SYS_UTHREAD_EXIT system call from thread %d!\n", thread_current()->tid);
+			th_status = ((int*)f->esp)[1];
+
+			process_uthread_status[thread_current()-> uthread_id] = th_status;
+
+			for(int i = 0; i <TH_NO; i++){
+				sema_up(&join_sema[thread_current()->uthread_id]);
+			}
+
+			// Terminate the thread
+			thread_exit();
+
+			f->eax=0;
+			return;
+		case SYS_UTHREAD_GETPID:
+			printf ("SYS_UTHREAD_GETPID system call from thread %d!\n", thread_current()->tid);
+
+			f->eax = thread_current()->pid;
+
+			return;
+		case SYS_UTHREAD_GETTID:
+			printf ("SYS_UTHREAD_GETTID system call from thread %d!\n", thread_current()->tid);
+
+			f->eax = thread_current()->uthread_id;
+
+			return;
 
     default:
       unexpected_exit();
